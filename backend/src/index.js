8b2817f9256bc19ef5df95e0e33867c8da94f7fb
aryan agrawal <aryanagrawal@aryans-MacBook-Air.local>
2026-02-04 11:47:04 +0530

    index.js changed after deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://echo-chat-second.vercel.app"
+    ],
     credentials: true,
   })
 );
@@ -32,14 +35,6 @@ app.use(
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 
 server.listen(PORT, () => {
